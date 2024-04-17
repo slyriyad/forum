bc96c595334fbb9370b45729302def1fc2d9af73
slyriyad <riyad25400@gmail.com>
2024-04-17 11:57:50 +0200

    affichege titre du topic dans les posts

--- forumPlateau_V2/view/forum/detailTopic.php
+++ forumPlateau_V2/view/forum/detailTopic.php
@@ -15,7 +15,7 @@
 </div>
 
 <div class="my-3 p-3 bg-white rounded box-shadow">
-  <h6 class="border-top border-black pb-2 mb-0"><h1 style="text-align: center;color: black">TITRE</h1>Post</h6>
+  <h6 class="border-top border-black pb-2 mb-0"><h1 style="text-align: center;color: black"><?= $topic->getTitle(); ?></h1>Post</h6>
   <?php foreach( $posts as $post ) { ?>
         <div class="media text-muted pt-3">
           
