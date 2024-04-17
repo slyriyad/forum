5cfaf4e8097174cb72cef660266bc7a965aa315a
slyriyad <riyad25400@gmail.com>
2024-04-17 16:57:27 +0200

    ajout register

--- forumPlateau_V2/controller/SecurityController.php
+++ forumPlateau_V2/controller/SecurityController.php
@@ -3,11 +3,81 @@ namespace Controller;
 
 use App\AbstractController;
 use App\ControllerInterface;
+use App\Session;
+use Model\Managers\CategoryManager;
+use Model\Managers\UserManager;
+use Model\Managers\TopicManager;
+use Model\Managers\PostManager;
 
 class SecurityController extends AbstractController{
     // contiendra les méthodes liées à l'authentification : register, login et logout
 
-    public function register () {}
+    public function register() {
+            return [
+                "view" => VIEW_DIR."security/register.php",
+                "meta_description" => "aze",
+                ];
+    }   
+            
+            
+
+    // public function addRegister() {
+    //     // créer une nouvelle instance de categoryManager
+    //     $userManager = new UserManager();
+    //     if (isset($_POST['add'])) {
+
+    //         $nickName = filter_input(INPUT_POST,"nickName", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //         $mail = filter_input(INPUT_POST,"mail", FILTER_SANITIZE_FULL_SPECIAL_CHARS,FILTER_VALIDATE_EMAIL);
+    //         $password = filter_input(INPUT_POST,"password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //         $password2 = filter_input(INPUT_POST,"password2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //         $role = 'user';
+    //         $registrationDate = date_default_timezone_set('Europe/Paris');
+    //         $registrationDate = date("Y-m-d H:i:s");
+            
+    //         if($nickName && $mail && $password1 && $password2){
+    //             var_dump("ok");die;
+    //         }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    //         // $data = [
+    //         //     'nickName' => $nickName,
+    //         //     'mail' => $mail,
+    //         //     'password' => $password,
+    //         //     'role' => $role,
+    //         //     'registrationDate' => $registrationDate
+    //         // ];
+
+    //         // $categories = $userManager->add($data);
+            
+    //     }
+    //         return [
+    //             "view" => VIEW_DIR."",
+    //             "meta_description" => "",
+    //             "data" => [
+    //                 ]
+    //             ];
+    //         }      
+    
+    
+    
+    
+    
+    
+    
+    
+    
     public function login () {}
     public function logout () {}
 }
---
+++ forumPlateau_V2/view/security/register.php
@@ -0,0 +1,22 @@
+<h1>S'inscrire</h1>
+
+<form action="index.php?ctrl=security&action=register" method="post">
+    <label for="name">NickName:</label><br>
+    <input type="text" placeholder="Nickname" name="nickName"><br>
+
+
+    <label for="name">E-mail:</label><br>
+    <input type="text" placeholder="E-mail" name="mail"><br>
+
+
+    <label for="name">Password:</label><br>
+    <input type="password" placeholder="password" name="password"><br>
+
+
+    <label for="name"> Confirm Password:</label><br>
+    <input type="password" placeholder="password" name="password2"><br>
+    
+    <input type="submit" value="add" class="bouton" name="add">
+
+    
+</form>
